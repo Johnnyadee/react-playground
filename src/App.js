ec31a276cde7cbc2c6b3408d81de1e5ecf062bb0
Johnnyadee <johnadeoluwai@yahoo.com>
2026-01-13 13:26:40 +0100

    12

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 // import React, { Component } from "react";0
 // import logo from "./logo.svg";
 import "./App.css";
+import Eventbind from "./Components/Eventbind";
 // import Greet from "./Components/Greet";
 // import Welcome from "./Components/Welcome";
 // import Hello from "./Components/Hello";
 // import Message from "./Components/Message";`
 // import ClickFunction from "./Components/ClickFunction";
-import ClickClass from "./Components/ClickClass";
+// import ClickClass from "./Components/ClickClass";
 
 function App() {
   return (
     <div className="App">
-      <ClickClass />
+      <Eventbind />
     </div>
   );
 }
---
+++ src/Components/Eventbind.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Eventbind extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Hello, this is the first message",
+    };
+  }
+
+  changeMessage() {
+    // this.setState({
+    //   message: "Hello, this is the second message",
+    // });
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.message}</p>
+        <button onClick={this.changeMessage}>Click Here</button>
+      </div>
+    );
+  }
+}
+
+export default Eventbind;
